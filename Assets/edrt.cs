25009e75e2fbdda732c6dde6740469e0d863098e
Jame Floc'h Le Carour <jame.flochlecarour@gmail.com>
2022-01-28 15:11:10 +0100

    new test

--- Assets/edrt.cs
+++ Assets/edrt.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class edrt : MonoBehaviour
 {
     [SerializeField] GameObject bossPrefab;
+    [SerializeField] GameObject bossPrefabVariant;
+    [SerializeField] Transform variantSpawn;
     [SerializeField] xsens.XsStreamReader actor;
 
     private Transform[] targetModel;
@@ -19,8 +21,13 @@ public class edrt : MonoBehaviour
         yield return new WaitForSeconds(1);
 
         GameObject boss = Instantiate(bossPrefab, transform);
+        GameObject bossVariant = Instantiate(bossPrefabVariant, variantSpawn);
+
+
         xsens.XsLiveAnimator ani = GetComponent<xsens.XsLiveAnimator>();
+        xsens.XsLiveAnimator anivariant = bossVariant.GetComponent<xsens.XsLiveAnimator>();
         ani.Setup(actor, boss.transform);
+        anivariant.Setup(actor, bossVariant.transform);
 
         
     }
