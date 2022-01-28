86a9fd67fa401964da3b3762252637c0cab36e73
Jame Floc'h Le Carour <jame.flochlecarour@gmail.com>
2022-01-28 14:59:49 +0100

    and again for maybe the last time ?

--- Assets/edrt.cs
+++ Assets/edrt.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class edrt : MonoBehaviour
 {
+    [SerializeField] GameObject bossPrefab;
     [SerializeField] xsens.XsStreamReader actor;
-    [SerializeField] Transform boss;
+
     private Transform[] targetModel;
     // Start is called before the first frame update
     void Start()
@@ -16,8 +17,10 @@ public class edrt : MonoBehaviour
     IEnumerator AddOtherScriptAfterAWhile()
     {
         yield return new WaitForSeconds(1);
+
+        GameObject boss = Instantiate(bossPrefab, transform);
         xsens.XsLiveAnimator ani = GetComponent<xsens.XsLiveAnimator>();
-        ani.Setup(actor, boss);
+        ani.Setup(actor, boss.transform);
 
         
     }
